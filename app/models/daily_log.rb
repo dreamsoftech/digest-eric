aec221c158746448cb40bf1ff5dfd69d9d2236c9
Kevin Reed <reedkevin88@gmail.com>
2013-08-03 18:53:02 +0800

    Modify server address and email receiver

--- app/models/daily_log.rb
+++ app/models/daily_log.rb
@@ -4,8 +4,43 @@ class DailyLog < ActiveRecord::Base
 
   Mailman::Rails.receive do
   	default do
-  		TaskMailer.receive(message)
+  		return if message.from.empty?
+	    
+	    member = Member.find_by_email(message.from.first)
+	    return if member.nil?
+
+	    if message.multipart?
+	        member.daily_logs.create(
+	            subject: message.subject,
+	            content: message.html_part.body.decoded
+	        )
+	    else
+	        member.daily_logs.create(
+	            subject: message.subject,
+	            content: message.body.decoded
+	        )
+	    end  		
   		puts message.inspect
   	end
+  	
+  end
+
+  def receive(email)
+    puts email
+
+    member = Member.find_by_email(email.from.first)
+    return if member.nil?
+
+    if email.multipart?
+        member.daily_logs.create(
+            subject: email.subject,
+            content: email.html_part.body.decoded
+        )
+    else
+        member.daily_logs.create(
+            subject: email.subject,
+            content: email.body.decoded
+        )
+    end
   end
 end
